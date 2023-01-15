8687c16099d68cd21e27d8b7b379f20b4013c4a9
Lawrence-Turner <lawrence.turner.lt94@gmail.com>
2023-01-15 20:09:26 +0000

    Added loop

--- Main/main.py
+++ Main/main.py
@@ -14,36 +14,43 @@ delta_t = 0.5
 max_t = 100
 num_t_steps = int(max_t / delta_t)
 
+num_scenarios = 1   #Num times repeated (different random scenario each time)
+#Variables for storing output on each iteration
+known_risk = np.zeros(num_scenarios)
+unknown_risk = np.zeros(num_scenarios)
 
-#Make initial scenario....
-[initial_positions, initial_velocities, end_points] = initial_scenario.generate_scenario(num_planes,grid_size)
+#Loop through num iterations
+for index in range(num_scenarios):
 
-#Random initial pos and direction etc
-#Random start & end points
+    #Make initial scenario....
+    [initial_positions, initial_velocities, end_points] = initial_scenario.generate_scenario(num_planes,grid_size)
 
-#Initialise aircraft objects in dict
-aircraft_dict = {}
-for i in range(num_planes):
-    #Generate arrays for initialising the Aircraft objects.=
-    pos_array = np.zeros((num_t_steps,3))
-    pos_array[0,:] = initial_positions[i,:]
-    vel_array = np.zeros((num_t_steps,3))
-    vel_array[0,:] = initial_velocities[i,:]
+    #Random initial pos and direction etc
+    #Random start & end points
 
-    #Call Aircraft class and stores instances in dict
-    aircraft_dict[i] = aircraft.Aircraft(i,pos_array,vel_array,end_points[i,:])
+    #Initialise aircraft objects in dict
+    aircraft_dict = {}
+    for i in range(num_planes):
+        #Generate arrays for initialising the Aircraft objects.=
+        pos_array = np.zeros((num_t_steps,3))
+        pos_array[0,:] = initial_positions[i,:]
+        vel_array = np.zeros((num_t_steps,3))
+        vel_array[0,:] = initial_velocities[i,:]
 
-    #Generate "known" paths from initial scenario
-    aircraft_dict[i].generate_known_path(delta_t,num_t_steps)
+        #Call Aircraft class and stores instances in dict
+        aircraft_dict[i] = aircraft.Aircraft(i,pos_array,vel_array,end_points[i,:])
 
-#print(aircraft_dict[1].position)
+        #Generate "known" paths from initial scenario
+        aircraft_dict[i].generate_known_path(delta_t,num_t_steps)
+
+    #print(aircraft_dict[1].position)
 
 
-#Calculate known risk from paths
-known_risk = risk.calc_known_risk(aircraft_dict)
+    #Calculate known risk from paths
+    known_risk[index] = risk.calc_known_risk(aircraft_dict)
 
 
-#Calculate unknown risk from 
-unknown_risk = risk.calc_unknown_risk(aircraft_dict)
+    #Calculate unknown risk from 
+    unknown_risk[index] = risk.calc_unknown_risk(aircraft_dict)
 
 #Compare or something
