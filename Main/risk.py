a8ad3d6a4a525e7b8138473145c8bacddbbe0d8f
Lawrence-Turner <lawrence.turner.lt94@gmail.com>
2023-01-15 20:12:44 +0000

    Structure in risk.py

---
+++ Main/risk.py
@@ -0,0 +1,10 @@
+def calc_known_risk(aircraft_dict):
+    #Placeholder - needs doing
+    known_risk = 0
+    return known_risk
+
+
+def calc_unknown_risk(aircraft_dict):
+    #Placeholder - needs doing
+    unknown_risk = 0
+    return unknown_risk
