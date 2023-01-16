523416fa25ca4c2ec2780acfbed8327217f22846
Lawrence-Turner <lawrence.turner.lt94@gmail.com>
2023-01-16 12:08:12 +0000

    new scenario structure

--- Main/scenario.py
+++ Main/scenario.py
@@ -1,11 +1,25 @@
 import numpy as np
 
+class Scenario:
+    def __init__(self):
+        pass
+        '''
+        Input vars like num_planes, delta_t etc
+        What attrs do we need?
+        Could have:
+        List/dict of plane objects
+        Different no. of crashed on paths
+        Avg no. crashed across paths
 
-def generate_scenario(num_planes,grid_size):
-    '''
-    '''
-    initial_positions = np.random.rand(num_planes,3) * grid_size
-    initial_velocities = np.random.rand(num_planes,3)
-    end_points = np.random.rand(num_planes,3) * grid_size
+        How to handle different random paths?
+        '''
 
-    return [initial_positions, initial_velocities, end_points]
+
+    def generate_scenario(num_planes,grid_size):
+        '''
+        '''
+        initial_positions = np.random.rand(num_planes,3) * grid_size
+        initial_velocities = np.random.rand(num_planes,3)
+        end_points = np.random.rand(num_planes,3) * grid_size
+
+        return [initial_positions, initial_velocities, end_points]
