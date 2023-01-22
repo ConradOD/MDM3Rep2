fc9b1e757ad522e16c9f68253fa938659c84e2ba
Lawrence-Turner <lawrence.turner.lt94@gmail.com>
2023-01-22 11:36:54 +0000

    Remove obselete code

--- Main/main.py
+++ Main/main.py
@@ -19,19 +19,14 @@ num_random_paths = 5
 
 num_scenarios = 20  #Num times repeated (different random scenario each time)
 
-separation_threshold = 5 #distance below which counts as loss of separation.
-
-
-#Variables for storing output on each iteration
-known_risk = np.zeros(num_scenarios)
-unknown_risk = np.zeros(num_scenarios)
+separation_threshold = 5 #Distance below which counts as loss of separation.
 
 #Dataframe for storing output
 data = pd.DataFrame([])
 
 #"Make data"
 for index in range(num_scenarios):
-    #make scenario
+    #Generate scenario
     scenario_object = scenario.Scenario(num_aircraft, num_t_steps, grid_size, delta_t, separation_threshold)
 
     #Initialise metrics
@@ -42,7 +37,6 @@ for index in range(num_scenarios):
     for name,val in metric_object.metrics_dict.items():
         data.loc[index,name] = val
 
-
     data.loc[index,'avg_crashed'] = risk.calc_known_risk(scenario_object,num_random_paths)  #Equivalent to known risk
 print(data.head())
 
@@ -52,15 +46,15 @@ y = data.iloc[:,-1]
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
 
 
-#Train model
+#Import sklearn functions
 from sklearn.linear_model import LinearRegression
-model = LinearRegression()
 from sklearn.metrics import mean_squared_error, r2_score
+
+#Initialise linear regression model
+model = LinearRegression()
+
+#Train model on train set
 model.fit(X_train,y_train)
-# classification_report()
-# y_pred = classifier.predict(X_test)
-# conf_matr = confusion_matrix(Y_test,y_pred)
-# print(conf_matr)
 
 #Predict using the trained model
 y_pred = model.predict(X_test)
@@ -71,6 +65,3 @@ print("Coefficients: \n", model.coef_)
 print("Mean squared error: %.2f" % mean_squared_error(y_test,y_pred))
 #The coefficient of determination: 1 is perfect prediction
 print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
-
-
-#Test model
